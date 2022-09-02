c6d3762a56a60fd0a14fa3c7fe6b294f54ff4838
Ravindra Singh Rathore <rravindrasingh168@gmail.com>
2022-09-03 03:13:36 +0530

    commit

--- data/db.js
+++ data/db.js
@@ -3,10 +3,9 @@ const mongoose = require('mongoose');
 const {NODE_ENV, DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
 // const DB_NAME = "EmailCommunicationEnginDB"
 
-const connectionStr = NODE_ENV === 'development' ? `mongodb://${DB_HOST}/${DB_NAME}` :  `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/?retryWrites=true&w=majority`;
+const connectionStr = NODE_ENV === 'development' ? `mongodb://${DB_HOST}/${DB_NAME}` :  `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/?retryWrites=true&w=majority`
 
-//mongodb+srv://emailcommunicationengine:<password>@cluster0.ebkgmgh.mongodb.net/?retryWrites=true&w=majority
-//mongodb+srv://emailcommunicationengine:<password>@cluster0.ebkgmgh.mongodb.net/test
+//mongodb+srv://emailcommunicationadmin:<password>@cluster0.8s75lvg.mongodb.net/?retryWrites=true&w=majority
 console.log("connectionStr", connectionStr);
 
 mongoose.connect(connectionStr, {
@@ -22,9 +21,3 @@ mongoose.connection.on('error', error => {
 mongoose.connection.on('open', function() {
     console.error(`connected to database ${DB_NAME}`)
 })
-
-//     
-
-// module.exports = {
-//     connect
-// }
