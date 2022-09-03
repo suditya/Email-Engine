eb3880e22422bd73ff768be28b8110d85d21ca24
Ravindra Singh Rathore <rravindrasingh168@gmail.com>
2022-09-04 03:00:41 +0530

    deploying

--- data/db.js
+++ data/db.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 
 const {NODE_ENV, DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
-// const DB_NAME = "EmailCommunicationEnginDB"
 
 const connectionStr = NODE_ENV === 'development' ? `mongodb://${DB_HOST}/${DB_NAME}` :  `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/?retryWrites=true&w=majority`
 
-//mongodb+srv://emailcommunicationadmin:<password>@cluster0.8s75lvg.mongodb.net/?retryWrites=true&w=majority
 
 mongoose.connect(connectionStr, {
     useNewUrlParser: true,
