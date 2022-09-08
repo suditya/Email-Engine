8d868250b10b19530c41b950a548cad1c59b1338
Ravindra Singh Rathore <rravindrasingh168@gmail.com>
2022-09-08 16:55:23 +0530

    final commit

--- controller/ScheduledEmailsController.js
+++ controller/ScheduledEmailsController.js
@@ -267,7 +267,6 @@ async function checkEmailEverySecond() {
 
             if (new Date(data).toISOString().slice(0, -5) === new Date().toISOString().slice(0, -5) && response.sent === false) {
 
-                await Emails.updateOne({ _id: id }, { sent: true })
 
                 console.log(new Date(data).toISOString().slice(0, -5));
                 console.log(new Date().toISOString().slice(0, -5));
@@ -307,7 +306,8 @@ async function checkEmailEverySecond() {
                 // console.log("newTransporter",newTransporter);
 
                 await newTransporter.sendMail(mailOptions)
-
+                await Emails.updateOne({ _id}, { sent: true })
+                
                 // console.log("sent");
             }
         })
