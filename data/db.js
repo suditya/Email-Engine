c73c2f24307b8145d14012b00e79f08b23ea73c5
suditya <70383984+suditya@users.noreply.github.com>
2023-02-19 16:24:07 +0530

    Update db.js

--- data/db.js
+++ data/db.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 const {NODE_ENV, DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
 
-const connectionStr = NODE_ENV === 'development' ? `mongodb://${DB_HOST}/${DB_NAME}` : `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/?retryWrites=true&w=majority`
+//const connectionStr = NODE_ENV === 'development' ? `mongodb://${DB_HOST}/${DB_NAME}` : `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/?retryWrites=true&w=majority`
+
+const connectionStr = NODE_ENV === 'development' ? `mongodb://${DB_HOST}/${DB_NAME}` : `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/?retryWrites=true&w=majority`
+
 
 
 mongoose.connect(connectionStr, {
